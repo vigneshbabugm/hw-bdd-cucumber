5569dda29f8e774f272a2f11153690b28f3518ba
EC2 Default User <ec2-user@ip-172-31-5-88.us-east-2.compute.internal>
2020-09-24 22:16:57 +0000

    part 3 completed, added log file

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -19,7 +19,9 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  #fail "Unimplemented"
+  regx=/.*#{e1}.*#{e2}.*/
+  expect(page.body.match(regx))
 end
 
 # Make it easier to express checking or unchecking several boxes at once
